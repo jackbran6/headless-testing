66ed1b6b3f850d612b38944c2f893d7e9ab51f63
Jack Branthwaite <jack@formfunfunction.com>
2021-08-17 15:57:34 +1200

    Basic structure created

---
+++ src/components/post-card/PostCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <article class="card">
+    <div class="card-shadow">
+      <div class="card-body">
+        <h5 class="card-title">
+          <NuxtLink :to="{ name: 'slug', params: { slug: post.slug } }">{{
+            post.title
+          }}</NuxtLink>
+        </h5>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <div class="card-text" v-html="post.excerpt"></div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: ['post']
+})
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 80vw;
+}
+.card-shadow {
+  border: 1px solid;
+  box-shadow: 5px 10px;
+  padding: 10px;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,34 +1,59 @@
 <template>
-  <Intro>
-    <Heading content="headless-testing" />
-    <p>
-      Welcome to the
-      <a
-        href="https://formfunfunction.com/"
-        target="_blank"
-        rel="noreferrer"
-      >formfunfunction</a>
-      Nuxt starter project. Head to<Code>/src/pages/index.vue</Code> to get
-      stuck in.
-    </p>
-  </Intro>
+  <div class="container">
+    <div>
+      <h1 class="title">Headless WordPress NuxtDemo</h1>
+      <div class="card-container">
+        <div class="row">
+          <post-card v-for="post in posts" :key="post.slug" :post="post" />
+          <div class="d-flex w-100">
+            <div v-if="pageInfo.hasNextPage" class="ml-auto">
+              <NuxtLink :to="{ query: { after: pageInfo.endCursor } }"
+                ><span>Next Page</span></NuxtLink
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import Intro from '@/components/intro/Intro.vue'
-import Code from '@/components/intro/Code.vue'
-import Heading from '@/components/intro/Heading.vue'
 
 export default Vue.extend({
   name: 'Home',
-  components: {
-    Intro,
-    Code,
-    Heading
-  },
 
   head: {
     title: 'Hello world!'
+  },
+  computed: {
+    posts() {
+      return [
+        {
+          title: 'Test Post',
+          slug: 'test-post',
+          content: '<p>This is a test post</p>',
+          excerpt: '<p>This is a test post</p>'
+        }
+      ]
+    },
+    pageInfo() {
+      return {
+        endCursor: '',
+        hasNextPage: false,
+        hasPreviousPage: false,
+        startCursor: ''
+      }
+    }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.title {
+  margin: 10vw;
+}
+.card-container {
+  margin-left: 10vw;
+}
+</style>
