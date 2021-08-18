fdcdb182248b7c10b8ec38be19fa99e3ebc574d9
Jack Branthwaite <jack@formfunfunction.com>
2021-08-18 12:45:03 +1200

    Creation of add new book page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container">
-    <div>
-      <h1 class="title">Headless WordPress NuxtDemo</h1>
+    <div class="div">
+      <div class="header-container">
+        <h1 class="title">Headless WordPress NuxtDemo</h1>
+        <nuxt-link to="new-book" class="add-container">
+          <p class="add-text">Add New</p>
+        </nuxt-link>
+      </div>
       <div class="card-container">
         <div class="row">
           <post-card v-for="post in posts" :key="post.id" :post="post" />
@@ -46,9 +51,25 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .title {
-  margin: 10vw;
+  margin: 50px;
 }
 .card-container {
   margin-left: 10vw;
+}
+.header-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.add-container {
+  border: 1px solid;
+  box-shadow: 10px 10px;
+  cursor: pointer;
+  display: flex;
+  height: 50px;
+  justify-content: center;
+  margin: 50px;
+  padding: 10px;
+  width: 10vw;
 }
 </style>
---
+++ src/pages/new-book.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <div class="add-form">
+      <h3 class="title">Book Title:</h3>
+      <textarea id="book-title" name="Book Title" class="title-container" />
+      <h3 class="publisher">Publisher:</h3>
+      <textarea id="publisher" name="Publisher" class="publisher-container" />
+      <h3 class="excerpt">Excerpt:</h3>
+      <textarea id="excerpt" name="Excerpt" class="excerpt-container" />
+      <h3 class="content">Content:</h3>
+      <textarea id="content" name="content" class="content-container" />
+      <div class="submit" @click="submit">
+        <p class="submit-text">Submit</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  methods: {
+    submit() {
+      console.log('Submit')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin-left: 10vw;
+  margin-right: 10vw;
+  margin-top: 100px;
+}
+.add-form {
+  justify-self: center;
+}
+.title-container {
+  border: 1px solid;
+  box-shadow: 10px 10px #485570;
+  cursor: pointer;
+  height: 50px;
+  margin-bottom: 50px;
+  margin-top: 10px;
+  padding: 10px;
+  width: 80vw;
+}
+.publisher-container {
+  border: 1px solid;
+  box-shadow: 10px 10px #485570;
+  cursor: pointer;
+  height: 50px;
+  margin-bottom: 50px;
+  margin-top: 10px;
+  padding: 10px;
+  width: 80vw;
+}
+.excerpt-container {
+  border: 1px solid;
+  box-shadow: 10px 10px #485570;
+  cursor: pointer;
+  height: 100px;
+  margin-bottom: 50px;
+  margin-top: 10px;
+  padding: 10px;
+  width: 80vw;
+}
+.content-container {
+  border: 1px solid;
+  box-shadow: 10px 10px #485570;
+  cursor: pointer;
+  height: 200px;
+  margin-bottom: 50px;
+  margin-top: 10px;
+  padding: 10px;
+  width: 80vw;
+}
+.submit {
+  border: 1px solid;
+  box-shadow: 10px 10px;
+  cursor: pointer;
+  display: flex;
+  height: 50px;
+  justify-content: center;
+  padding: 10px;
+  width: 10vw;
+}
+h3 {
+  line-height: 1;
+  margin: 0;
+  padding: 0;
+}
+</style>
